b5c276567201d5e720cd968594b395623a4096e9
zero8511 <thoughtzero8@gmail.com>
2025-10-07 02:06:09 +0530

    Update ping_script.py
    Changed Url

--- ping_script.py
+++ ping_script.py
@@ -5,7 +5,7 @@ import logging
 
 def ping_endpoints():
     endpoints = [ 
-"https://zero-thought-node.onrender.com/"
+"https://zero-thought-node-1.onrender.com/"
     ]
     
     for url in endpoints:
